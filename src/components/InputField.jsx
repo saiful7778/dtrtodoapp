2551b6c9302c02547841a011cbcd1d73c4a47fb9
Saiful islam <saiful.islam.rafi.88@gmail.com>
2023-09-30 23:18:41 +0600

    update

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
 
-const InputField = ({ type, placeholder }) => {
+const InputField = ({ value, onChange, type, placeholder }) => {
   return (
     <div className="relative h-11">
-      <input className="input-box peer" type={type || "text"} placeholder="" />
+      <input
+        value={value}
+        onChange={onChange}
+        className="input-box peer"
+        type={type || "text"}
+        placeholder=""
+      />
       <label className="input-box-label">{placeholder}</label>
     </div>
   );
 };
 
 InputField.propTypes = {
+  value: PropTypes.any,
+  onChange: PropTypes.func,
   placeholder: PropTypes.string,
   type: PropTypes.string,
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,39 +1,56 @@
 import { Link } from "react-router-dom";
-import InputField from "../components/InputField.jsx";
-import NavigateButton from "../components/NavigateButton.jsx";
+import InputField from "../components/InputField";
+import NavigateButton from "../components/NavigateButton";
+import useInputState from "../hooks/useInputState";
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useInputState("");
+  const [lastName, setLastName] = useInputState("");
+
   const handleSignUp = (e) => {
     e.preventDefault();
-  }
+    console.log(firstName, lastName);
+  };
 
   return (
-    <div className="container-box w-full md:w-3/4 mx-auto">
-      <h3 className="text-3xl font-semibold text-center">Sign Up</h3>
-      <div className="space-y-3">
-      <form className="flex flex-col gap-4 p-6" onSubmit={handleSignUp}>
-        <div className="flex gap-4">
-          <InputField placeholder="First Name" type="text" />
-          <InputField placeholder="Last Name" type="text" />
+    <>
+      <div className="container-box w-full md:w-3/4 mx-auto">
+        <h3 className="text-3xl font-semibold text-center">Sign Up</h3>
+        <div className="space-y-3">
+          <form className="flex flex-col gap-4 p-6" onSubmit={handleSignUp}>
+            <div className="flex gap-4">
+              <InputField
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
+                type="text"
+              />
+              <InputField
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
+                type="text"
+              />
+            </div>
+            <InputField placeholder="Email" type="email" />
+            <InputField placeholder="Password" type="password" />
+            <button className="btn btn-pri p-3" type="submit">
+              Sign up
+            </button>
+          </form>
+          <p className="leading-3 text-sm text-center">
+            Do you have an account?{" "}
+            <Link
+              className="font-bold text-blue-500 underline"
+              to="/getaccess/signin"
+            >
+              Sign in
+            </Link>
+          </p>
+          <NavigateButton />
         </div>
-        <InputField placeholder="Email" type="email" />
-        <InputField placeholder="Password" type="password" />
-        <button className="btn btn-pri p-3" type="submit">
-          Sign up
-        </button>
-        </form>
-        <p className="leading-3 text-sm text-center">
-          Do you have an account?{" "}
-          <Link
-            className="font-bold text-blue-500 underline"
-            to="/getaccess/signin"
-          >
-            Sign in
-          </Link>
-        </p>
-        <NavigateButton />
       </div>
-    </div>
+    </>
   );
 };
 
